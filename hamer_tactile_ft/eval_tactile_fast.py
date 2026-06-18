4228bdb36942b3e784ffbfc70089782b7f7db323
jiangrui <ljr050516@outlook.com>
2026-06-18 15:27:46 +0000

    fix: set PYOPENGL_PLATFORM to osmesa to prevent headless server segfaults

--- hamer_tactile_ft/eval_tactile_fast.py
+++ hamer_tactile_ft/eval_tactile_fast.py
@@ -1,5 +1,8 @@
 import sys
 import os
+# Force OSMesa to prevent silent EGL segfaults on headless servers
+os.environ['PYOPENGL_PLATFORM'] = 'osmesa'
+import json
 import argparse
 import numpy as np
 import torch
